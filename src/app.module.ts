1af5b6cb334e1a62480a2a00ddd5e3bb3a912c9e
Gabriel Pablo González Fresard <legab@Gabriels-MacBook-Pro.local>
2023-02-10 02:31:17 -0300

    ss

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,17 @@
 import { Module } from '@nestjs/common'
 import { AppController } from './app.controller'
-import { SwitchModule } from './switch/switch.module';
+import { SwitchModule } from './switch/switch.module'
+import { WhatsappModule } from './whatsapp/whatsapp.module'
+import { ConfigModule } from '@nestjs/config'
 
 @Module({
-  imports: [SwitchModule],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true
+    }),
+    SwitchModule,
+    WhatsappModule
+  ],
   controllers: [AppController]
 })
 export class AppModule {}
